182ce0bd33032ba93a3265216df965b5d45724e6
sygmo <sydney.mercier91@gmail.com>
2017-02-03 10:25:28 -0600

    added timestamps to surveys

--- server/models/poll.js
+++ server/models/poll.js
@@ -26,7 +26,7 @@ var PollSchema = new mongoose.Schema({
 		required: true,
 		minlength: [3, "Option must be at least 3 characters"]
 	}
-});
+}, { timestamps: true });
 
 
 mongoose.model('Poll', PollSchema);
